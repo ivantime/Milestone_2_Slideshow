6e5feafd2fee3a49bc1360aa389162401afafb77
ivantime <sayivanthreetimes@gmail.com>
2022-08-03 05:45:36 +0800

    re-editted Milestone 2's Slides

---
+++ script.js
@@ -0,0 +1,28 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 200;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+function revealHover() {
+    var reveals = document.querySelectorAll(".revealHover");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 500;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+window.addEventListener("scroll", reveal);
+window.addEventListener("scroll", revealHover);
